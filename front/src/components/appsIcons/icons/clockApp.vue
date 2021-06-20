da274261299a1a2fa8d20e16cb68edb5a1b663b3
Constantine Gololobov <dev@gololobov.dev>
2021-06-20 12:33:13 +0200

    Clock App Icon Base

--- front/src/components/appsIcons/icons/clockApp.vue
+++ front/src/components/appsIcons/icons/clockApp.vue
@@ -12,45 +12,74 @@ foreignObject.html-icon.icon-glare(x="0" y="0")
 	height: 100%
 	background: #000
 	.clock-body
+		
 		width: 80px
 		height: 80px
 		border-radius: 50%
 		background: #fff
 		margin: 10px auto
 		position: relative
-		&:before
-			content: ''
-			position: absolute
-			top: 0
-			right: 0
-			bottom: 0
-			left: 0
-			margin: auto
+		// &:before
+		// 	content: ''
+		// 	width: 10px
+		// 	height: 10px
+		// 	background: #fff
+		// 	border: 2px solid #000
+		// 	border-radius: 50%
+		// 	position: absolute
+		// 	top: calc(50% - 5px)
+		// 	left: calc(50% - 5px)
+		// 	z-index: 1
+		
+		.clock-hand
+			backface-visibility: hidden
 			width: 10px
 			height: 10px
+			position: absolute
+			top: calc(50% - 5px)
+			left: calc(50% - 5px)
 			border: 2px solid #000
 			border-radius: 50%
-			z-index: 2
-		.clock-hand
-			position: absolute
-			// top: 0
-			bottom: 5px
-			left: calc(50% - 1px)
-			margin: auto
-			transform-origin: 0px
+			&:before
+				content: ''
+				position: absolute
+			
 		.clock-h-hand
-			width: 2px
-			height: 20px
-			background: #000
-			transform: rotate(150deg)
-		.clock-s-hand
-			width: 2px
-			height: 50%
-			background: red
+			animation: 10s tick infinite
+			&:before
+				width: 2px
+				height: 12px
+				background: #000
+				bottom: 6px
+				left: 2px
 		.clock-m-hand
-			width: 2px
-			height: 50%
-			background: blue
-			transform: rotate(50deg)
+			animation: 5s tick infinite
+			&:before
+				width: 2px
+				height: 20px
+				background: #000
+				bottom: 6px
+				left: 2px
+		.clock-s-hand
+			animation: 1s tick infinite
+			width: 4px
+			height: 4px
+			border: none
+			background: red
+			top: calc(50% - 2px)
+			left: calc(50% - 2px)
+			&:before
+				width: 2px
+				height: 35px
+				background: red
+				bottom: 1px
+				left: 1px
 		
+		
+	
+	@keyframes tick
+		from
+			transform: rotate(0deg)
+		to
+			transform: rotate(360deg)
 </style>
