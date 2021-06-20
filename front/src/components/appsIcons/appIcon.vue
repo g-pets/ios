f01054c5f87f4f7d0ce83310227f106e193c75c2
Constantine Gololobov <dev@gololobov.dev>
2021-06-20 19:23:36 +0200

    Clock Build

--- front/src/components/appsIcons/appIcon.vue
+++ front/src/components/appsIcons/appIcon.vue
@@ -3,9 +3,9 @@
 	svg.app-icon(xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000")
 		component(:is="name")
 	.app-title {{title}}
-.app-item.png
-	img.app-icon(:src="`/img/apps/${name}.png`")
-	.app-title {{title}}
+//- .app-item.png
+//- 	img.app-icon(:src="`/img/apps/${name}.png`")
+//- 	.app-title {{title}}
 </template>
 
 <script>
